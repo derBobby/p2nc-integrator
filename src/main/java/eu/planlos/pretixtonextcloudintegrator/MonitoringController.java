05aaf2a00ad1ba389c5828da4aa1f9b21bbaf604
derBobby <dev@planlos.eu>
2023-05-15 20:51:47 +0200

    Revert "Commit to test a failing start for docker-compose"
    This reverts commit 0b7ef57057968a40102d839414bad521bc66fa22.

--- src/main/java/eu/planlos/pretixtonextcloudintegrator/MonitoringController.java
+++ src/main/java/eu/planlos/pretixtonextcloudintegrator/MonitoringController.java
@@ -1,15 +1,11 @@
 package eu.planlos.pretixtonextcloudintegrator;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class MonitoringController {
-
-    @Value("${not.existing}")
-    String failStart;
 
     @GetMapping("/health")
     public ResponseEntity<String> health() {

318b2254e1441f0fbe68e750b71a124f8ac6a1ca
derBobby <dev@planlos.eu>
2023-10-19 23:55:42 +0200

    Add Repository Test

---
+++ src/test/java/eu/planlos/pretixtonextcloudintegrator/pretix/repository/BookingRepositoryIntegrationTest.java
@@ -0,0 +1,37 @@
+package eu.planlos.pretixtonextcloudintegrator.pretix.repository;
+
+import eu.planlos.pretixtonextcloudintegrator.pretix.model.Booking;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+@ExtendWith(SpringExtension.class)
+@DataJpaTest
+class BookingRepositoryIntegrationTest {
+
+    @Autowired
+    private BookingRepository bookingRepository;
+
+    @Test
+    public void injectedComponentsAreNotNull() {
+        assertThat(bookingRepository).isNotNull();
+    }
+
+    @Test
+    public void insertDuplicate_throwsException() {
+        Booking firstBooking = new Booking("first event", "XC0DE", "Firstname", "Lastname", "email@example.com", LocalDateTime.now(), new ArrayList<>());
+        Booking secondBooking = new Booking("second event", "XC0DE", "Firstname", "Lastname", "email@example.com", LocalDateTime.now(), new ArrayList<>());
+
+        bookingRepository.saveAndFlush(firstBooking);
+        assertThrows(DataIntegrityViolationException.class, () -> bookingRepository.saveAndFlush(secondBooking));
+    }
+}

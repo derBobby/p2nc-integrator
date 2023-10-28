723f3f48283505b707bfe5104a62bd821ea6f30a
derBobby <dev@planlos.eu>
2023-10-29 01:57:36 +0200

    Logging

--- src/main/java/eu/planlos/pretixtonextcloudintegrator/pretix/config/PretixApiConfig.java
+++ src/main/java/eu/planlos/pretixtonextcloudintegrator/pretix/config/PretixApiConfig.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Profile;
 
 import java.util.List;
 
+//TODO organizer not yet used from filter, authentication?
 @ConfigurationProperties(prefix = "pretix.api")
 @Profile("!TEST")
 public record PretixApiConfig(boolean active, String address, String token, String organizer, List<String> eventList) {
--- src/main/java/eu/planlos/pretixtonextcloudintegrator/pretix/controller/WebHookValidationErrorHandler.java
+++ src/main/java/eu/planlos/pretixtonextcloudintegrator/pretix/controller/WebHookValidationErrorHandler.java
@@ -1,5 +1,6 @@
 package eu.planlos.pretixtonextcloudintegrator.pretix.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.FieldError;
@@ -8,10 +9,13 @@ import org.springframework.web.server.ResponseStatusException;
 import java.util.HashMap;
 import java.util.Map;
 
+@Slf4j
 public class WebHookValidationErrorHandler {
 
     public static void handle(BindingResult bindingResult) {
+
         if (bindingResult.hasErrors()) {
+            log.info("Validation failed for request.");
 
             Map<String, String> validationErrors = new HashMap<>();
             for (FieldError error : bindingResult.getFieldErrors()) {
@@ -26,5 +30,6 @@ public class WebHookValidationErrorHandler {
             // Return the error response with a 400 Bad Request status
             throw new ResponseStatusException(HttpStatus.BAD_REQUEST, errorResponse.toString());
         }
+        log.debug("Validation successfull for request");
     }
 }

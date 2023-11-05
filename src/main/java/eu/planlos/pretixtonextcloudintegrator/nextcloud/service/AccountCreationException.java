bd53dc61e81f473ce57ec02e993737f5e7d4e386
derBobby <dev@planlos.eu>
2023-11-05 23:16:04 +0100

    Remove Nextcloud code in favour of library

--- src/main/java/eu/planlos/pretixtonextcloudintegrator/nextcloud/service/AccountCreationException.java
+++
@@ -1,7 +0,0 @@
- package eu.planlos.pretixtonextcloudintegrator.nextcloud.service;
-
- public class AccountCreationException extends RuntimeException {
-     public AccountCreationException(String message) {
-        super(message);
-    }
-}

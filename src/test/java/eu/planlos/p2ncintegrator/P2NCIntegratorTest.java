13f3c35b9fb271ff2fc9e3e57bdc4d06ffe5595f
derBobby <dev@planlos.eu>
2023-12-10 23:53:59 +0100

    Test clean up

--- src/test/java/eu/planlos/p2ncintegrator/P2NCIntegratorTest.java
+++ src/test/java/eu/planlos/p2ncintegrator/P2NCIntegratorTest.java
@@ -1,13 +1,11 @@
 package eu.planlos.p2ncintegrator;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.ContextConfiguration;
 
 @SpringBootTest
-@AutoConfigureMockMvc
 @ActiveProfiles("TEST")
 @ContextConfiguration(classes = {TestConfig.class}) // Add this line to specify the test context configuration
 class P2NCIntegratorTest {

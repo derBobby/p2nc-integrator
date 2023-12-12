3a13c28a036ec5ad8f8b90549d0de1c062a8da23
derBobby <dev@planlos.eu>
2023-12-12 23:35:47 +0100

    Fix test

--- src/main/java/eu/planlos/p2ncintegrator/config/PretixConfig.java
+++ src/main/java/eu/planlos/p2ncintegrator/config/PretixConfig.java
@@ -11,7 +11,7 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
  */
 @Configuration
 @ComponentScan(basePackages = "eu.planlos.javapretixconnector")
-@EnableJpaRepositories(basePackages = {"eu.planlos.javapretixconnector"})
-@EntityScan(basePackages = "eu.planlos.javapretixconnector.model")
+@EnableJpaRepositories(basePackages = {"eu.planlos.javapretixconnector", "eu.planlos.p2ncintegrator"})
+@EntityScan(basePackages = {"eu.planlos.javapretixconnector", "eu.planlos.p2ncintegrator"})
 public class PretixConfig {
 }

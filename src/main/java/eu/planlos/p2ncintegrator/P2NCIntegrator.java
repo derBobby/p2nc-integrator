e2264feaf98d8f8eec311f65d90ce9c764725455
derBobby <dev@planlos.eu>
2023-12-16 17:41:48 +0100

    Revert "Update according to README of java-pretix-connector library"
    This reverts commit 1cd7e24fd37fd4c1782022bf491cd5076f94aa91.

--- src/main/java/eu/planlos/p2ncintegrator/P2NCIntegrator.java
+++ src/main/java/eu/planlos/p2ncintegrator/P2NCIntegrator.java
@@ -2,14 +2,10 @@ package eu.planlos.p2ncintegrator;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
 @ConfigurationPropertiesScan
-@EntityScan(basePackages = {"eu.planlos.p2ncintegrator"})
-@EnableJpaRepositories(basePackages = {"eu.planlos.p2ncintegrator"})
 public class P2NCIntegrator {
 
     public static void main(String[] args) {
